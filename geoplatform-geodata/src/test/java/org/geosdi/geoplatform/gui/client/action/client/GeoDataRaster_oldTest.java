e70327e11661da9bb2f523d6f770c4bdf2a00f56
massimocostantini <massimo.costantini@gmail.com>
2012-11-16 11:15:57 +0100

    MC: added class

---
+++ geoplatform-geodata/src/test/java/org/geosdi/geoplatform/gui/client/action/client/GeoDataRaster_oldTest.java
@@ -0,0 +1,48 @@
+/**
+* ------------------------------------------------------------------
+* -- Copyright © <Creation_date>-<Last modification date> SELEX Sistemi
+* -- Integrati S.p.A. all rights reserved.
+* -- This software is the property of SELEX Sistemi Integrati S.p.A.
+* -- and can not be reproduced, used to prepare Derivative Works of,
+* -- publicly displayed, publicly performed, sublicensed, and it
+* -- cannot be distributed as the Work itself and such Derivative
+* -- Works in Source or Object form except under a license agreement
+* -- granted by SELEX Sistemi Integrati S.p.A.
+* ------------------------------------------------------------------
+*
+* @file GeoDataRaster_oldTest.java
+* @author mc
+* @date 09/nov/2012
+* @brief
+* @date Date ; Name ; Description of modification
+*/
+package org.geosdi.geoplatform.gui.client.action.client;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * @author mc
+ *
+ */
+public class GeoDataRaster_oldTest {
+
+	/**
+	 * Test method for {@link org.geosdi.geoplatform.gui.client.action.client.GeoDataRaster_old#GeoDataRaster_old()}.
+	 */
+	@Test
+	public void testGeoDataRaster_old() {
+		
+		assertTrue(true);
+	}
+
+	/**
+	 * Test method for {@link org.geosdi.geoplatform.gui.client.action.client.GeoDataRaster_old#componentSelected(com.extjs.gxt.ui.client.event.MenuEvent)}.
+	 */
+	@Test
+	public void testComponentSelectedMenuEvent() {
+		assertTrue(true);
+	}
+
+}
