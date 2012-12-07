6b7311d22e25d28887a94adbb1e3f37a850a95ce
massimocostantini <massimo.costantini@gmail.com>
2012-12-07 17:20:19 +0100

    MC mgerg with geosdi

---
+++ geoplatform-services/geoplatform-modules/geoplatform-publisher/publisher-impl/src/main/java/snippet/Snippet.java
@@ -0,0 +1,39 @@
+/**
+* ------------------------------------------------------------------
+* -- Copyright © <Creation_date>-<Last modification date> SELEX Sistemi
+* -- Integrati S.p.A. all rights reserved.
+* -- This software is the property of SELEX Sistemi Integrati S.p.A.
+* -- and can not be reproduced, used to prepare Derivative Works of,
+* -- publicly displayed, publicly performed, sublicensed, and it
+* -- cannot be distributed as the Work itself and such Derivative
+* -- Works in Source or Object form except under a license agreement
+* -- granted by SELEX Sistemi Integrati S.p.A.
+* ------------------------------------------------------------------
+*
+* @file Snippet.java
+* @author mc
+* @date 23/nov/2012
+* @brief
+* @date Date ; Name ; Description of modification
+*/
+package snippet;
+
+/**
+ * @author mc
+ *
+ */
+public class Snippet {
+	/**
+	 * @description
+	 * @param 
+	 * @return 
+	 * @exception 
+	 * @see 
+	 * 23/nov/2012
+	 * @author mc
+	 * modified:
+	 */
+	public static void main(String[] args) {
+	}
+}
+
